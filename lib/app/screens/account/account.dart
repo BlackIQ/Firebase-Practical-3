cad85481dc52cfda77d99679147183a4d6713d9f
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-12 00:51:51 +0330

    Working on index

--- lib/app/screens/account/account.dart
+++ lib/app/screens/account/account.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AccountScreen extends StatefulWidget {
   @override
@@ -12,8 +13,288 @@ class _AccountScreenState extends State<AccountScreen> {
       appBar: AppBar(
         title: Text('Manage accounts'),
       ),
-      body: Center(
-        child: Text('Login'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 150,
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.solidEnvelope,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Authentication',
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                      Text(
+                        'Email',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 150,
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.solidEnvelope,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Authentication',
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                      Text(
+                        'Email',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 150,
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.solidEnvelope,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Authentication',
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                      Text(
+                        'Email',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 150,
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.solidEnvelope,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Authentication',
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                      Text(
+                        'Email',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 150,
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.google,
+                        color: Colors.white,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Authentication',
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                      Text(
+                        'Google',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 150,
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.facebook,
+                        color: Colors.white,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Authentication',
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                      Text(
+                        'Email',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 150,
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.solidEnvelope,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Authentication',
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                      Text(
+                        'Email',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 150,
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.solidEnvelope,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Authentication',
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                      Text(
+                        'Email',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 150,
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.solidEnvelope,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Authentication',
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                      Text(
+                        'Email',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
