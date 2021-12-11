2cdc711ed6877d0a38f931cf2c737503a3bbf157
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-11 23:15:48 +0330

    Add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:multifirebaseauthentication/app/models/user.dart';
 import 'package:multifirebaseauthentication/app/screens/landing.dart';
+import 'package:multifirebaseauthentication/app/services/authentication.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Multi Firebase Authentication',
-      theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-      ),
-      home: Landing(),
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      builder: (context, snapshot) {
+        return MaterialApp(
+          title: 'Multi Firebase Authentication',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+          ),
+          home: Landing(),
+        );
+      }
     );
   }
 }
