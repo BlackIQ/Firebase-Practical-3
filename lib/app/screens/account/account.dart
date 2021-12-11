7417be8d6116063355532f3ff6f0694b3e8a5bf1
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-11 23:02:05 +0330

    Create screen page

---
+++ lib/app/screens/account/account.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AccountScreen extends StatefulWidget {
+  @override
+  _AccountScreenState createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends State<AccountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Manage accounts'),
+      ),
+      body: Center(
+        child: Text('Login'),
+      ),
+    );
+  }
+}
