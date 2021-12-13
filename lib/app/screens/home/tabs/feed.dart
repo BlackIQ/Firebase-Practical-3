efe61bf9325357159b669df06ca24bde0d2fa140
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-13 23:11:06 +0330

    Remove useless imports

--- lib/app/screens/home/tabs/feed.dart
+++ lib/app/screens/home/tabs/feed.dart
@@ -1,15 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:multifirebaseauthentication/app/models/user.dart';
 import 'package:multifirebaseauthentication/app/services/database.dart';
 import 'package:multifirebaseauthentication/app/widgets/posts/screen_post.dart';
-import 'package:provider/provider.dart';
 
 class FeedTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final User user = Provider.of<User>(context);
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -23,7 +18,8 @@ class FeedTab extends StatelessWidget {
                   itemCount: snapshot.data.documents.length,
                   itemBuilder: (context, index) {
                     return ScreenSinglePost(
-                        post: snapshot.data.documents[index]);
+                      post: snapshot.data.documents[index],
+                    );
                   },
                 );
               } else {
