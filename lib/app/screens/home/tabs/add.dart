954f6d56b4891ecedc63471b8f6448f74116575d
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-13 23:10:48 +0330

    Rempve title field

--- lib/app/screens/home/tabs/add.dart
+++ lib/app/screens/home/tabs/add.dart
@@ -1,12 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:multifirebaseauthentication/app/models/user.dart';
-import 'package:multifirebaseauthentication/app/services/database.dart';
 import 'package:multifirebaseauthentication/app/widgets/buttons/wide_button.dart';
 import 'package:multifirebaseauthentication/app/widgets/fields/wide_field.dart';
-import 'package:multifirebaseauthentication/app/widgets/posts/screen_post.dart';
-import 'package:provider/provider.dart';
 
 class AddTab extends StatelessWidget {
   @override
@@ -14,7 +8,6 @@ class AddTab extends StatelessWidget {
     TextEditingController _title = TextEditingController();
     TextEditingController _content = TextEditingController();
 
-    final User user = Provider.of<User>(context);
     return SingleChildScrollView(
       padding: EdgeInsets.all(25),
       child: Column(
@@ -22,18 +15,9 @@ class AddTab extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           WideField(
-            lines: 1,
-            controller: _title,
-            hint: 'Title of the post',
-            label: 'Title',
-            obsecure: false,
-          ),
-          SizedBox(height: 10),
-          WideField(
-            lines: 5,
             controller: _content,
             hint: 'Write your post content',
-            label: 'Text',
+            label: 'Post',
             obsecure: false,
           ),
           SizedBox(height: 10),
